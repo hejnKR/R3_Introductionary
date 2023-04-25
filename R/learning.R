3f994ec2c1758282144589e135789971e5c1acb3
Kamilla Holtmann Hejn <kamillahejn@gmail.com>
2023-04-25 15:38:05 +0200

    First save to History

---
+++ R/learning.R
@@ -0,0 +1,16 @@
+# Basics_of_R -------------------------------------------------------------
+
+colnames(airquality)
+
+str(airquality)
+
+summary(airquality)
+
+
+
+mean(2:6)
+
+
+# Loading Packages --------------------------------------------------------
+
+library(tidyverse)
